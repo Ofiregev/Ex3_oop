72f16cec63f0e22f588a2c4069e5a3e24ba68c4d
avgilboa <you@example.comgilboa.aviad@gmail.com>
2021-12-23 10:58:42 +0200

    TSP work

--- src/guiG.py
+++ src/guiG.py
@@ -30,10 +30,9 @@ def arrow(start, end, d, h):
     x = xn * cos - yn * sin + start[0]
     yn = xn * sin + yn * cos + start[1]
     xn = x
-    points = [(end[0], end[1]), (int(xm), int(ym)), (int(xn), int(yn))]
-
-    pygame.draw.line([screen.get_width(), screen.get_height()], (0, 0, 0), start, end)
-    pygame.draw.polygon([screen.get_width(), screen.get_height()], (0, 0, 0), points)
+    points = [(end[0]-3, end[1]-3), (int(xm)-3, int(ym)-3), (int(xn)-3, int(yn)-3)]
+    pygame.draw.line(screen, pygame.Color(240,240,240),start,end,width=1)
+    pygame.draw.polygon(screen,pygame.Color(240,240,240),points)
 
 
 
@@ -73,13 +72,16 @@ while True:
         pos = n.pos.split(",")
         x = scale(float(pos[0]),30,screen.get_width()-30,min_x,max_x)
         y = scale(float(pos[1]),20,screen.get_height()-20,min_y,max_y)
-        pygame.draw.circle(screen,pygame.Color(7,0,0),(x,y),8)
         for e in g.g.all_out_edges_of_node(n.id):
             other_pos = g.g.graphDict.get(e).pos.split(',')
             o_x = scale(float(other_pos[0]),30,screen.get_width()-30,min_x,max_x)
             o_y = scale(float(other_pos[1]), 20, screen.get_height()-20, min_y, max_y)
-            arrow([])
-            ##pygame.draw.line(screen,pygame.Color(240,240,240), (x,y),(o_x,o_y),width=2)
+            arrow([x,y],[o_x,o_y],25,10)
+    for n in g.g.graphDict.values():
+        pos = n.pos.split(",")
+        x = scale(float(pos[0]), 30, screen.get_width() - 30, min_x, max_x)
+        y = scale(float(pos[1]), 20, screen.get_height() - 20, min_y, max_y)
+        pygame.draw.circle(screen, pygame.Color(7, 0, 0), (x, y), 8)
         id_srf = FONT.render(str(n.id),True, pygame.Color(255,11,249))
         rect = id_srf.get_rect(center=(x, y-5))
         screen.blit(id_srf, rect)
